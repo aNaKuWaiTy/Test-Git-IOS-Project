f9a8fc44196f11cb8a119b9e732f39ddb66c200e
aNaKuWaiTy <iaNaKuWaiTy@aNaKuWaiTys-MacBook-Pro.local>
2014-04-14 02:10:08 +0300

    added a Test class

---
+++ Git test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git test
+//
+//  Created by aNaKuWaiTy on 4/14/14.
+//  Copyright (c) 2014 aNaKuWaiTy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git test
+//
+//  Created by aNaKuWaiTy on 4/14/14.
+//  Copyright (c) 2014 aNaKuWaiTy. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
