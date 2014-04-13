0634d8900d78b08c1b530ff78c21e86041703a4f
aNaKuWaiTy <iaNaKuWaiTy@aNaKuWaiTys-MacBook-Pro.local>
2014-04-13 18:23:56 +0300

    Initial Commit

---
+++ Git test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git test
+//
+//  Created by aNaKuWaiTy on 4/13/14.
+//  Copyright (c) 2014 aNaKuWaiTy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Git test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git test
+//
+//  Created by aNaKuWaiTy on 4/13/14.
+//  Copyright (c) 2014 aNaKuWaiTy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
